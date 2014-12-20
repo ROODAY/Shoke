25ff661c3c179aa53e2a2913fe2af1cdc4b4b45d
ROODAY <rudytheninja@gmail.com>
2014-12-19 23:09:29 -0500

    betterized ui, implemented spotify oauth

--- index.js
+++ index.js
@@ -51,16 +51,12 @@ app.get('/', function(req, res){
   res.render('index.html', { user: req.user });
 });
 
-app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account.html', { user: req.user });
-});
-
 app.get('/login', function(req, res){
   res.render('login.html', { user: req.user });
 });
 
 app.get('/auth/spotify',
-  passport.authenticate('spotify', {scope: 'user-read-email'}),
+  passport.authenticate('spotify', {scope: 'playlist-read-private'}),
   function(req, res){
 });
 
